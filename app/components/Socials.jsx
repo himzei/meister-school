2091426c0c9b34d38d0983f9621ea4dbbdeb5a69
조현일 <himzei@gmail.com>
2024-05-13 23:19:24 +0900

    only kakao

--- app/components/Socials.jsx
+++ app/components/Socials.jsx
@@ -1,19 +1,18 @@
 import { Button } from "@/components/ui/button";
 import { RiKakaoTalkFill } from "react-icons/ri";
-import { FaGoogle, FaGithub } from "react-icons/fa";
-import { SiNaver } from "react-icons/si";
+
 import Link from "next/link";
 
 export default function Socials() {
   return (
-    <div className="grid grid-cols-4 gap-2 *:uppercase">
+    <div className="flex w-full *:uppercase">
       <Button variant="outline" asChild>
-        <Link href="/kakao/start" className="flex gap-x-1">
+        <Link href="/kakao/start" className="flex w-full">
           <RiKakaoTalkFill size={22} />
           카카오
         </Link>
       </Button>
-      <Button variant="outline" asChild>
+      {/* <Button variant="outline" asChild>
         <Link href="/naver/start" className="flex gap-x-1">
           <SiNaver size={16} />
           네이버
@@ -31,7 +30,7 @@ export default function Socials() {
           <FaGithub size={20} />
           깃헙
         </Link>
-      </Button>
+      </Button> */}
     </div>
   );
 }
